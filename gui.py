5ece613281310e79543342f7bdd4498869ce6456
JdoubleW <k.evin.b@outlook.com>
2017-10-23 18:45:44 +0200

    Layout opgebouwd. Moet nog aangepast worden. Niet alle knoppen werken. Talen knop werkt.

--- gui.py
+++ gui.py
@@ -1,54 +1,129 @@
 from tkinter import *
 
-root = Tk()
+def showMainFrameNL():
+    mainframeENG.pack_forget()
+    mainframeNL.pack()
 
-mainmenu = Frame(master=root)
-mainmenu.pack(fill="both", expand=True)
-label = Label(master=root,
+def showMainFrameENG():
+    mainframeNL.pack_forget()
+    mainframeENG.pack()
+
+def clickedENG():
+    showMainFrameENG()
+
+def clickedNL():
+    showMainFrameNL()
+
+
+##################################################################################
+
+
+root = Tk()
+mainframeNL = Frame(master=root)          #Start mainframe
+mainframeNL.pack(fill="both", expand=True)
+
+label = Label(master=mainframeNL,
                text='Welkom bij NS',
                background='yellow',
-               foreground='blue',
+               foreground='#3333FF',
                font=('', 40, ''),
                width=40,
                height=12)
 
-button1 = Button(master=root,
+button1 = Button(master=mainframeNL,
                 text='Weergeef vertrektijden \n van Utrecht Centraal',
                 foreground='white',
-                background='blue',
+                background='#3333FF',
                 width=20,
                 height=4)
 
-button2 = Button(master=root,
+button2 = Button(master=mainframeNL,
                 text='Weergeef vertrektijden \n van Utrecht Centraal',
                 foreground='white',
-                background='blue',
+                background='#3333FF',
                 width=20,
                 height=4)
 
-button3 = Button(master=root,
+button3 = Button(master=mainframeNL,
                 text='Weergeef vertrektijden \n van Utrecht Centraal',
                 foreground='white',
-                background='blue',
+                background='#3333FF',
                 width=20,
                 height=4)
 
-button4 = Button(master=root,
+button4 = Button(master=mainframeNL,
                 text='Weergeef vertrektijden \n van Utrecht Centraal',
                 foreground='white',
-                background='blue',
+                background='#3333FF',
                 width=20,
                 height=4)
 
-button_dutch = Button(master=root,
-                foreground='white',
-                background='blue',
-                width=20,
+button_ENG = Button(master=mainframeNL,
+                command=clickedENG,
+                background='#3333FF',
+                width=10,
                 height=4)
 label.pack()
 button1.place(x=310, y=550)
 button2.place(x=480, y=550)
 button3.place(x=650, y=550)
 button4.place(x=820, y=550)
-button_dutch.place(y=655)
+button_ENG.place(x=0, y=655)
+
+##################################################################################
+
+mainframeENG = Frame(master=root)
+mainframeENG.pack(fill="both", expand=True)
+
+label = Label(master=mainframeENG,
+               text='Welcome to NS',
+               background='yellow',
+               foreground='#3333FF',
+               font=('', 40, ''),
+               width=40,
+               height=12)
+
+button1 = Button(master=mainframeENG,
+                text='Show departure times \n from Utrecht Centraal',
+                foreground='white',
+                background='#3333FF',
+                width=20,
+                height=4)
+
+button2 = Button(master=mainframeENG,
+                text='Weergeef vertrektijden \n van Utrecht Centraal',
+                foreground='white',
+                background='#3333FF',
+                width=20,
+                height=4)
+
+button3 = Button(master=mainframeENG,
+                text='Weergeef vertrektijden \n van Utrecht Centraal',
+                foreground='white',
+                background='#3333FF',
+                width=20,
+                height=4)
+
+button4 = Button(master=mainframeENG,
+                text='Weergeef vertrektijden \n van Utrecht Centraal',
+                foreground='white',
+                background='#3333FF',
+                width=20,
+                height=4)
+
+button_NL = Button(master=mainframeENG,
+                command=clickedNL,
+                background='#3333FF',
+                width=10,
+                height=4)
+
+
+label.pack()
+button1.place(x=310, y=550)
+button2.place(x=480, y=550)
+button3.place(x=650, y=550)
+button4.place(x=820, y=550)
+button_ENG.place(x=0, y=655)
+button_NL.place(x=0, y=655)
+showMainFrameNL()
 root.mainloop()
